e807627d6a5da9bb6d5e0c5f91232be5f73c2c3d
Hill <hill@kimo.com>
2018-09-13 06:58:47 +0000

    export i13nDispatcher

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@
 export { default as i13nStore} from './stores/i13nStore';
 
 // Dispatch
-export { i13nDispatch } from './i13nDispatcher';
+export { default as i13nDispatcher, i13nDispatch } from './i13nDispatcher';

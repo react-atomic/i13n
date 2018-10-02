e4ceef19b3514882394e1b820ddabf85a33f8ef2
Hill <hill@kimo.com>
2018-10-02 16:31:24 +0800

    better ie support

--- packages/client/src/req.js
+++ packages/client/src/req.js
@@ -1,5 +1,6 @@
 const req = (url, callback) => {
-  const request = XDomainRequest ? XDomainRequest : XMLHttpRequest;
+  const request =
+    'undefined' !== typeof XDomainRequest ? XDomainRequest : XMLHttpRequest;
   const oReq = new request();
   oReq.onload = callback(oReq);
   oReq.open('GET', url);

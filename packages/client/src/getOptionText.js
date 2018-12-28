cc8a19530c301433c51d15c6d0102e43844c5cb0
Hill <hill@kimo.com>
2018-12-28 23:28:43 +0800

    improve getOptionText

--- packages/client/src/getOptionText.js
+++ packages/client/src/getOptionText.js
@@ -1,13 +1,13 @@
-import query from 'css-query-selector';
+import query, {queryFrom} from 'css-query-selector';
 import get from 'get-object-value';
 
 const getOptionText = sel => {
   const thisSel = query.el(sel);
-  if (!sel) {
+  if (!thisSel) {
     return;
   }
   const val = get(thisSel, ['value'], '');
-  return get(thisSel.querySelector("option[value='" + val + "']"), ['text'], '');
+  return get(queryFrom(thisSel).one("option[value='" + val + "']"), ['text'], '');
 };
 
 export default getOptionText;

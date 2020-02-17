003ff954edb22384a8442945fefa6e0e9279c927
Hill <hill@kimo.com>
2020-02-17 18:15:19 +0800

    Add delegate debug

--- packages/i13n-client/src/delegate.js
+++ packages/i13n-client/src/delegate.js
@@ -14,21 +14,22 @@ const delegate = (el, type, childs, defaultFunc) => {
   query.el(el).addEventListener(type, e => {
     const t = e.target;
     childs.some(({select, func}) => {
-      const doms = query.all(select);
-      if (!doms.length) {
-        return false;
-      }
       if (FUNCTION !== typeof func) {
         func = defaultFunc;
       }
-      return doms.some(childEl => {
-        if (t.isSameNode(childEl) || childEl.contains(t)) {
-          callfunc(func, [{...e, currentTarget: childEl}, e]);
-          return true;
-        } else {
-          return false;
-        }
-      });
+      if ('debug' === select) {
+        return callfunc(func, [e]);
+      } else {
+        const doms = query.all(select);
+        return (doms || []).some(childEl => {
+          if (t.isSameNode(childEl) || childEl.contains(t)) {
+            callfunc(func, [{...e, currentTarget: childEl}, e]);
+            return true;
+          } else {
+            return false;
+          }
+        });
+      }
     });
   });
 };

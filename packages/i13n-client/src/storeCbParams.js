e1bb2d86f0c99b38aa5f1dfc90839cb8be49380a
Hill <hill@kimo.com>
2020-02-19 17:01:52 +0800

    fixed empty event

--- packages/i13n-client/src/storeCbParams.js
+++ packages/i13n-client/src/storeCbParams.js
@@ -1,11 +1,12 @@
 import {i13nDispatch} from 'i13n';
+import get from 'get-object-value';
 
 const _LAST_EVENT = 'lastEvent';
 const _I13N_CB_PARAMS = 'i13nCbParams';
 
 const storeCbParams = (params, e) => {
   i13nDispatch({
-    [_LAST_EVENT]: [e, e.currentTarget],
+    [_LAST_EVENT]: [e, get(e, ['currentTarget'])],
     [_I13N_CB_PARAMS]: params,
   });
 };

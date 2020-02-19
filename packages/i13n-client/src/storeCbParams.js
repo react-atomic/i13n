f9f03859cdf56618b2aac2cbfcac9f301f77e821
Hill <hill@kimo.com>
2020-02-19 18:51:29 +0800

    fixed should not overwrite last event, when event is empty

--- packages/i13n-client/src/storeCbParams.js
+++ packages/i13n-client/src/storeCbParams.js
@@ -5,10 +5,14 @@ const _LAST_EVENT = 'lastEvent';
 const _I13N_CB_PARAMS = 'i13nCbParams';
 
 const storeCbParams = (params, e) => {
-  i13nDispatch({
-    [_LAST_EVENT]: [e, get(e, ['currentTarget'])],
-    [_I13N_CB_PARAMS]: params,
-  });
+  const arr = {};
+  if (params) {
+    arr[_I13N_CB_PARAMS] = params;
+  }
+  if (e) {
+    arr[_LAST_EVENT] = [e, get(e, ['currentTarget'])];
+  }
+  i13nDispatch(arr);
 };
 
 export default storeCbParams;

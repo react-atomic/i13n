817a42323ee1bc40c98b9079787ca83afa068137
Hill <hill@kimo.com>
2020-05-13 13:46:53 +0800

    better pass event

---
+++ packages/i13n-client/src/__tests__/delegateTest.js
@@ -0,0 +1,28 @@
+import { expect } from "chai";
+import gjsdom from "jsdom-global";
+import query from "css-query-selector";
+
+import delegate from "../delegate";
+
+describe("Test delegate", () => {
+  let reset;
+  beforeEach(() => {
+    reset = gjsdom(null, { url: "http://localhost" });
+  });
+
+  afterEach(() => {
+    reset();
+  });
+
+  it("basic test", done => {
+    document.body.innerHTML = `
+      <div class="test"></div>
+    `;
+    delegate(document.body, "click", ".test", e => {
+      expect(e.target.className).to.equal('test');
+      expect(e.currentTarget.className).to.equal('test');
+      done();
+    });
+    query.one('.test').dispatchEvent(new Event("click", {bubbles: true}));
+  });
+});
--- packages/i13n-client/src/delegate.js
+++ packages/i13n-client/src/delegate.js
@@ -1,6 +1,6 @@
-import query from 'css-query-selector';
-import callfunc from 'call-func';
-import {FUNCTION} from 'reshow-constant';
+import query from "css-query-selector";
+import callfunc from "call-func";
+import { FUNCTION } from "reshow-constant";
 
 const isArray = Array.isArray;
 
@@ -9,21 +9,28 @@ const delegate = (el, type, childs, defaultFunc) => {
     return;
   }
   if (!isArray(childs)) {
-    childs = [{select: childs}];
+    childs = [{ select: childs }];
   }
   query.el(el).addEventListener(type, e => {
     const t = e.target;
-    childs.some(({select, func}) => {
+    childs.some(({ select, func }) => {
       if (FUNCTION !== typeof func) {
         func = defaultFunc;
       }
-      if ('debug' === select) {
+      if ("debug" === select) {
         return callfunc(func, [e]);
       } else {
-        const doms = query.all(select);
+        const doms = query.from(el).all(select);
         return (doms || []).some(childEl => {
           if (t.isSameNode(childEl) || childEl.contains(t)) {
-            callfunc(func, [{...e, currentTarget: childEl, nativeEvent: e}]);
+            callfunc(func, [
+              {
+                type: e.type,
+                target: e.target,
+                currentTarget: childEl,
+                nativeEvent: e
+              }
+            ]);
             return true;
           } else {
             return false;

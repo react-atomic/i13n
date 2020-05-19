d77f6dd5bfe1bd8f794db4c0cf3a5579b4492ab5
Hill <hill@kimo.com>
2020-05-19 23:39:28 +0800

    lazyAttr add expire support

--- packages/i13n-client/src/lazyAttr.js
+++ packages/i13n-client/src/lazyAttr.js
@@ -1,18 +1,34 @@
-import {toMap} from 'get-object-value';
-import {UNDEFINED} from 'reshow-constant';
-import {sStore} from './storage'; 
+import { toMap } from "get-object-value";
+import { UNDEFINED } from "reshow-constant";
+import { sStore } from "./storage";
 
-const lazyKey = 'lazyAttr';
-const lazyAttr = key => value => {
+const lazyKey = "i13nLazyAttr";
+const expireKey = "i13nLazyExpire";
+
+const lazyAttr = (key, expireSec) => value => {
   const arr = toMap(sStore.get(lazyKey));
   if (UNDEFINED === typeof key) {
     return arr;
   }
+  const expireArr = toMap(sStore.get(expireKey));
+  const now = new Date().getTime();
   if (UNDEFINED !== typeof value) {
     arr[key] = value;
+    expireArr[key] = now;
     sStore.set(lazyKey, arr);
+    sStore.set(expireKey, expireArr);
   }
-  return arr[key];
+  const createTime = expireArr[key];
+  const userExpire = expireSec * 1000;
+  let isUpToDate = true;
+  if (!isNaN(userExpire) && !isNaN(createTime)) {
+    if (now - createTime >= userExpire) {
+      isUpToDate = false;
+    }
+  }
+  if (isUpToDate) {
+    return arr[key];
+  }
 };
 
 export default lazyAttr;

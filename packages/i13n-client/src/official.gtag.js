0cf4c267b844d37b46aa4f5df263cdb28d93cc13
Hill <hill@kimo.com>
2020-05-24 14:24:02 +0800

    improve new date

--- packages/i13n-client/src/official.gtag.js
+++ packages/i13n-client/src/official.gtag.js
@@ -1,12 +1,12 @@
-import exec from 'exec-script';
-import {win} from 'win-doc';
-import {getTimestamp} from 'get-random-id';
+import exec from "exec-script";
+import { win } from "win-doc";
+import { getTimestamp } from "get-random-id";
 
-import BaseGTag from './BaseGTag';
+import BaseGTag from "./BaseGTag";
 
 const getScript = gtagId => {
-  const script = ` 
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  const script = `<script>
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   ${getTimestamp()},event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
@@ -17,7 +17,7 @@ const getScript = gtagId => {
 
 class OfficialGTag extends BaseGTag {
   init() {
-    const {id} = this.props; 
+    const { id } = this.props;
     exec(getScript(id));
   }
 

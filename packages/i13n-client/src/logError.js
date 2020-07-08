6554b228baf76f52f16b51ecf38dfe371fa4003f
Hill <hill@kimo.com>
2020-07-08 13:52:45 +0800

    add maxError

--- packages/i13n-client/src/logError.js
+++ packages/i13n-client/src/logError.js
@@ -4,8 +4,16 @@ import {getLastScript} from 'exec-script';
 import {doc} from 'win-doc';
 
 let debugFlag = false;
+let errorCount = 0;
+const maxError = 5;
 
 const logError = (error, action, name) => {
+  if (errorCount === maxError) {
+    console.log('Max Errors exceed.', maxError);
+  } else if (errorCount > maxError) {
+    return;
+  }
+  errorCount++;
   let {message, stack} = error || {};
   stack = get(error, ['stack'], '').split(/\n/);
   const wait = action && -1 !== action.indexOf('I13nScript') ? 0 : undefined;

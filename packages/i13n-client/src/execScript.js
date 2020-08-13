d4b5b28bff6aaff4f310714f4bb7b33c7a8a52ad
Hill <hill@kimo.com>
2020-08-13 08:55:13 +0800

    Add dump code when error happen

--- packages/i13n-client/src/execScript.js
+++ packages/i13n-client/src/execScript.js
@@ -1,12 +1,16 @@
 import i13nStore from "i13n-store";
 import exec from "exec-script";
 import logError from "./logError";
+
+const SCRIPT_ERROR = "I13nScriptErr";
+
 const execScript = (scriptName) => {
   const scriptCode = i13nStore.getState().get("script")?.get(scriptName);
   if (scriptCode) {
-    exec(scriptCode, null, null, (e) =>
-      logError(e, "I13nScriptErr", scriptName)
-    );
+    exec(scriptCode, null, null, (e) => {
+      console.log(SCRIPT_ERROR, [scriptCode]);
+      logError(e, SCRIPT_ERROR, scriptName);
+    });
   } else {
     console.warn("Script: [" + scriptName + "] not found.");
   }
